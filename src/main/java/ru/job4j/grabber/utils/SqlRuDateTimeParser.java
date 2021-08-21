be1e3fc96249c712f7d00b8196a367b65af7e819
mvladlen <vladlen@ufanet.ru>
2021-08-21 07:53:05 +0500

    2.1. Преобразование даты [#289476 #192098] fix

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -1,5 +1,6 @@
 package ru.job4j.grabber.utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Map;
 
@@ -9,29 +10,48 @@ public class SqlRuDateTimeParser implements DateTimeParser {
 
     private static final Map<String, String> MONTHS = Map.ofEntries(
             entry("янв", "1"),
-            entry( "фев","2"),
-            entry( "мар","3"),
-            entry( "апр","4"),
-            entry( "май","5"),
-            entry( "июн","6"),
-            entry( "июл","7"),
-            entry( "авг","8"),
-            entry( "сен","9"),
-            entry( "окт","10"),
-            entry( "ноя","11"),
-            entry( "дек","12")
+            entry("фев", "2"),
+            entry("мар", "3"),
+            entry("апр", "4"),
+            entry("май", "5"),
+            entry("июн", "6"),
+            entry("июл", "7"),
+            entry("авг", "8"),
+            entry("сен", "9"),
+            entry("окт", "10"),
+            entry("ноя", "11"),
+            entry("дек", "12")
     );
 
     @Override
     public LocalDateTime parse(String parse) {
 
         String[] strings = parse.split("[\\s,:]");
+        if (strings.length < 6)
+            if (strings[0].equals("сегодня")) {
+                return LocalDateTime.of(
+                        LocalDate.now().getYear(),
+                        LocalDate.now().getMonth(),
+                        LocalDate.now().getDayOfMonth(),
+                        Integer.parseInt(strings[2]),
+                        Integer.parseInt(strings[3]));
+
+            } else if (strings[0].equals("вчера")) {
+                return LocalDateTime.of(
+                        LocalDate.now().getYear(),
+                        LocalDate.now().getMonth(),
+                        LocalDate.now().getDayOfMonth(),
+                        Integer.parseInt(strings[2]),
+                        Integer.parseInt(strings[3])).minusDays(1);
+
+            }
         return LocalDateTime.of(
-                Integer.parseInt(strings[2]),
+                2000+Integer.parseInt(strings[2]),
                 Integer.parseInt(MONTHS.get(strings[1])),
                 Integer.parseInt(strings[0]),
                 Integer.parseInt(strings[4]),
                 Integer.parseInt(strings[5])
-                );
+        );
+
     }
 }
--- src/test/java/ru/job4j/grabber/utils/SqlRuDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/SqlRuDateTimeParserTest.java
@@ -10,11 +10,30 @@ import static org.junit.Assert.*;
 public class SqlRuDateTimeParserTest {
 
     @Test
-    public void whenDate ()
+    public void whenDateTime ()
     {
         String date = "12 авг 21, 12:34";
         SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
         LocalDateTime dateTime = parser.parse(date);
-        assertEquals(dateTime.toString(),"0021-08-12T12:34");
+        assertEquals(dateTime.toString(),"2021-08-12T12:34");
     }
+    @Test
+    public void whenDateToday ()
+    {
+        String date = "сегодня, 12:34";
+        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+        LocalDateTime dateTime = parser.parse(date);
+        assertNotNull(dateTime);
+    }
+
+    @Test
+    public void whenDateYesterday ()
+    {
+        String date = "вчера, 12:34";
+        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+        LocalDateTime dateTime = parser.parse(date);
+        assertNotNull(dateTime);
+    }
+
+
 }
