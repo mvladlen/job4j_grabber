245c28b02995f74af07cd33e49e8d9a209e3257c
mvladlen <vladlen@ufanet.ru>
2021-08-21 09:04:47 +0500

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/utils/Post.java
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -8,4 +8,44 @@ public class Post {
     String link; // типа String - ссылка на описание вакансии;
     String description; // типа String - описание вакансии;
     LocalDateTime created; // типа LocalDateTime - дата создания вакансии.
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,19 +4,75 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.Post;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
+    List<Post> posts = new LinkedList<>();
+    int page = 0;
 
-        for (int i = 1; i <=5 ; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+    public List<Post> SqlRuParsePages(int pages) {
+
+        for (int i = 1; i <= pages; i++) {
+            getNextPage();
+        }
+        return posts;
+    }
+    private List<String> getTopic(String link) {
+        Document doc;
+        List<String> messages = new LinkedList<>();
+        try {
+            doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".msgBody");
+            for (Element td: row) {
+                messages.add(td.ownText());
+            }
+
+        } catch (IOException e) {
+            System.out.println("can't open page");
+        }
+        return messages;
+    }
+    private List<Post> getNextPage() {
+        page++;
+        Document doc;
+        DateTimeParser timeParser = new SqlRuDateTimeParser();
+        try {
+            doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
+                Post post = new Post();
                 Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(td.parent().children().get(5).text());
+                post.setLink(href.attr("href"));
+                post.setTitle(href.text());
+                post.setCreated(timeParser.parse(td.parent().children().get(5).text()));
+                List<String> messages = getTopic(post.getLink());
+                post.setDescription(messages.get(1));
+                posts.add(post);
+            }
+        } catch (IOException e) {
+            System.out.println("can't open page");
+        }
+        return posts;
+    }
+
+        public static void main(String[] args) {
+
+        SqlRuParse parser = new SqlRuParse();
+        for (int i = 1; i <= 5; i++) {
+            List<Post> posts = parser.getNextPage();
+            for (Post post : posts) {
+                System.out.println(post.getTitle());
+                System.out.println(post.getDescription());
+                System.out.println(post.getLink());
+                System.out.println(post.getCreated());
             }
         }
     }
+
 }
