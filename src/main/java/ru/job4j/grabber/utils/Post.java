5b216c34ee3997eefe744968a4e63ea716270882
mvladlen <vladlen@ufanet.ru>
2021-08-23 18:30:39 +0500

    3. Архитектура проекта - Аргегатор Java Вакансий [#260359]

--- src/main/java/ru/job4j/grabber/utils/Post.java
+++ src/main/java/ru/job4j/grabber/utils/Post.java
@@ -76,13 +76,12 @@ public class Post {
         Post post = (Post) o;
         return Objects.equals(title, post.title) &&
                 Objects.equals(link, post.link) &&
-                Objects.equals(description, post.description) &&
                 Objects.equals(created, post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link, description, created);
+        return Objects.hash(title, link, created);
     }
 
     @Override
